d530c38e84ca17086f8c2f040f7a53b6557ede63
licai <1981824361@qq.com>
2025-08-05 09:12:57 +0800

    feat: 安卓启动图和图标

--- android/app/src/main/java/com/awesomern/MainActivity.java
+++ android/app/src/main/java/com/awesomern/MainActivity.java
@@ -3,8 +3,15 @@ package com.awesomern;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import org.devio.rn.splashscreen.SplashScreen;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);  // 显示启动屏幕
+        super.onCreate(savedInstanceState);
+    }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
